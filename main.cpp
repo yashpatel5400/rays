4ccf3d30194fad36b10d3a1f4889bd083d415fb3
yashpatel5400 <ypatel5400@gmail.com>
2020-07-04 11:40:00 -0700

    Fix shadow calculation

--- main.cpp
+++ main.cpp
@@ -25,24 +25,63 @@ struct Sphere {
     cv::Vec3b m_color;
     float m_radius;
 
-    Sphere(glm::vec3 center,
-           cv::Vec3b color,
-           float radius) :
+    Sphere(const glm::vec3& center,
+        const cv::Vec3b& color,
+        const float radius) :
         m_center(center),
         m_color(color),
         m_radius(radius) {}
 };
 
+struct Ray {
+    glm::vec3 m_head;
+    glm::vec3 m_dir; // normalize before init
+
+    Ray(const glm::vec3& head,
+        const glm::vec3& dir) :
+        m_head(head),
+        m_dir(dir) {}
+};
+
 using Scene = std::vector<Sphere>; // assume only spheres for simplicity
 
-cv::Vec3b raytrace(const Scene& scene, const glm::vec3& light, const glm::vec3& dir) {
+// we return glm::vec3(-1) to indicate no intersection
+glm::vec3 intersectSphere(const Sphere& sphere, const Ray& ray) {
+    // starting the ray offset is the same as displacing the world in the opposite direction
+    glm::vec3 offsetCenter = sphere.m_center - ray.m_head;
+
+    float det =
+        4.0 * (pow(sphere.m_radius, 2.0) - pow(glm::length(offsetCenter), 2.0))
+        + 4.0 * pow(glm::dot(ray.m_dir, offsetCenter), 2.0);
+    if (det >= 0.0) {
+        float intersectionTime1 = -0.5 * sqrt(det) + glm::dot(ray.m_dir, offsetCenter);
+        float intersectionTime2 = 0.5 * sqrt(det) + glm::dot(ray.m_dir, offsetCenter);
+
+        // want the smaller of the two > 0
+        float intersectionTime = (intersectionTime1 < 0 && intersectionTime2 > 0)
+            ? std::max(intersectionTime1, intersectionTime2)
+            : std::min(intersectionTime1, intersectionTime2);
+
+        if (intersectionTime >= 0.0) {
+            return ray.m_head + intersectionTime * ray.m_dir;
+        }
+    }
+    return glm::vec3(-1);
+}
+
+bool inShadow(const Scene& scene, const Ray& ray) {
     for (const Sphere& sphere : scene) {
-        float det = 
-            4.0 * (pow(sphere.m_radius, 2.0) - pow(glm::length(sphere.m_center), 2.0))
-            + 4.0 * pow(glm::dot(dir, sphere.m_center), 2.0);
-        if (det >= 0.0) {
-            float intersectionTime = -0.5 * sqrt(det) + glm::dot(dir, sphere.m_center);
-            glm::vec3 intersection = dir * intersectionTime;
+        if (intersectSphere(sphere, ray) != glm::vec3(-1)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+cv::Vec3b raytrace(const Scene& scene, const glm::vec3& light, const Ray& ray) {
+    for (const Sphere& sphere : scene) {
+        glm::vec3 intersection = intersectSphere(sphere, ray);
+        if (intersection != glm::vec3(-1)) {
             glm::vec3 normal = glm::normalize(intersection - sphere.m_center);
             glm::vec3 lightRay = glm::normalize(light - intersection);
 
@@ -51,8 +90,13 @@ cv::Vec3b raytrace(const Scene& scene, const glm::vec3& light, const glm::vec3& 
                 return cv::Vec3b(128 + normal.x * 127, 128 + normal.y * 127, 128 + normal.z * 127);
             }
 
-            float brightness = 1000.0 / glm::length(light - intersection);
-            return sphere.m_color * brightness;
+            const float kSmidgen = .05; // we offset from the intersection to avoid self-shadow detection
+            Ray shadowRay(intersection + lightRay * kSmidgen, lightRay);
+            if (inShadow(scene, shadowRay)) {
+                return cv::Vec3b(0, 127, 0);
+            }
+
+            return sphere.m_color;
         }
     }
     return cv::Vec3b(0, 0, 0);
@@ -73,7 +117,8 @@ int main(int argc, char** argv) {
         200.0
     );
 
-    glm::vec3 light(100.0, 100.0, 300.0);
+    glm::vec3 light(500.0, 500.0, 300.0);
+    glm::vec3 origin(0.0);
 
     float focal = 100.0; // assumed camera focal in pixels
     cv::Mat output(cv::Size(FLAGS_width, FLAGS_height), CV_8UC3);
@@ -82,7 +127,8 @@ int main(int argc, char** argv) {
             glm::vec3 dir = glm::normalize(
                 glm::vec3(x - FLAGS_width / 2, y - FLAGS_height / 2, focal)
             );
-            output.at<cv::Vec3b>(y, x) = raytrace(scene, light, dir);
+            Ray ray(origin, dir);
+            output.at<cv::Vec3b>(y, x) = raytrace(scene, light, ray);
         }
     }
 
